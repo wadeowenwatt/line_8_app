5ff4cd135c0380693926dc2d29e33aac01d2b11c
Linhtn1 <linhtn1@newwave.vn>
2022-12-27 18:25:13 +0700

    navigate to wiki webview

--- lib/ui/pages/new_home/new_home_page.dart
+++ lib/ui/pages/new_home/new_home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base/router/route_config.dart';
 import 'package:flutter_base/ui/pages/new_home/widgets/base_info_widget.dart';
 import 'package:flutter_base/ui/pages/new_home/widgets/big_double_card_widget.dart';
 import 'package:flutter_base/ui/pages/new_home/widgets/double_card_widget.dart';
+import 'package:get/get.dart';
 
 import '../../../common/app_colors.dart';
+
 part 'method.dart';
 
 const List choices = [
@@ -15,14 +18,14 @@ const List choices = [
       pathImage: "assets/images/ic_fake_2.png",
       bigText: "300\$",
       smallText: "Amount"),
-  Choice(title: "2", pathImage: "assets/images/img.png"),
-  Choice(title: "3", pathImage: "assets/images/img_1.png"),
-  Choice(title: "4", pathImage: "assets/images/img_2.png"),
-  Choice(title: "5", pathImage: "assets/images/img_3.png"),
-  Choice(title: "6", pathImage: "assets/images/img_4.png"),
-  Choice(title: "7", pathImage: "assets/images/img_5.png"),
-  Choice(title: "8", pathImage: "assets/images/img_6.png"),
-  Choice(title: "9", pathImage: "assets/images/img_7.png"),
+  Choice(title: "Wiki", pathImage: "assets/images/img.png", pathScreen: RouteConfig.wiki),
+  Choice(title: "3", pathImage: "assets/images/img_1.png", pathScreen: ""),
+  Choice(title: "4", pathImage: "assets/images/img_2.png", pathScreen: ""),
+  Choice(title: "5", pathImage: "assets/images/img_3.png", pathScreen: ""),
+  Choice(title: "6", pathImage: "assets/images/img_4.png", pathScreen: ""),
+  Choice(title: "7", pathImage: "assets/images/img_5.png", pathScreen: ""),
+  Choice(title: "8", pathImage: "assets/images/img_6.png", pathScreen: ""),
+  Choice(title: "9", pathImage: "assets/images/img_7.png", pathScreen: ""),
 ];
 
 class NewHomePage extends StatelessWidget {
@@ -34,9 +37,9 @@ class NewHomePage extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
-          AppColors.primaryDarkColorLeft,
-          AppColors.primaryLightColorRight
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+              AppColors.primaryDarkColorLeft,
+              AppColors.primaryLightColorRight
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         child: Column(
           children: [
             const InformationWidget(),
@@ -51,15 +54,24 @@ class NewHomePage extends StatelessWidget {
                           choice1: choices[index], choice2: choices[index + 1]);
                     }
                     return RowTwoCardSelection(
-                        choice1: choices[index * 2],
-                        choice2: (index * 2 + 1) >= choices.length
-                            ? null
-                            : choices[index * 2 + 1]);
+                      choice1: choices[index * 2],
+                      choice2: (index * 2 + 1) >= choices.length
+                          ? null
+                          : choices[index * 2 + 1],
+                      onClick1: () {
+                        _onClick1((choices[index * 2] as Choice).pathScreen);
+                      },
+                      onClick2: () {
+                        (index * 2 + 1) >= choices.length ? {} :
+                        _onClick2(
+                            (choices[index * 2 + 1] as Choice).pathScreen);
+                      },
+                    );
                   },
                   padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
                   itemCount: (choices.length / 2).round(),
                 )
-              ]),
+              ],),
             ),
           ],
         ),
@@ -67,4 +79,11 @@ class NewHomePage extends StatelessWidget {
     );
   }
 
+  void _onClick1(String path) {
+    if (path != "") Get.toNamed(path);
+  }
+
+  void _onClick2(String path) {
+    /// Todo
+  }
 }
--- lib/ui/pages/new_home/widgets/double_card_widget.dart
+++ lib/ui/pages/new_home/widgets/double_card_widget.dart
@@ -5,15 +5,24 @@ import '../../../../common/app_colors.dart';
 class Choice {
   final String title;
   final String pathImage;
+  final String pathScreen;
 
-  const Choice({required this.title, required this.pathImage});
+  const Choice(
+      {required this.title, required this.pathImage, required this.pathScreen});
 }
 
 class RowTwoCardSelection extends StatelessWidget {
-  const RowTwoCardSelection({Key? key, required this.choice1, this.choice2})
-      : super(key: key);
+  const RowTwoCardSelection({
+    Key? key,
+    required this.choice1,
+    this.choice2,
+    required this.onClick1,
+    required this.onClick2,
+  }) : super(key: key);
   final Choice choice1;
   final Choice? choice2;
+  final VoidCallback onClick1;
+  final VoidCallback onClick2;
 
   @override
   Widget build(BuildContext context) {
@@ -22,28 +31,33 @@ class RowTwoCardSelection extends StatelessWidget {
         Expanded(
           child: SizedBox(
             height: 130,
-            child: Card(
-              color: AppColors.selectCardColor,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      choice1.pathImage,
-                      height: 45,
-                      width: 40,
-                      fit: BoxFit.fill,
-                    ),
-                    Text(
-                      choice1.title,
-                      style: const TextStyle(color: Colors.black),
-                    )
-                  ],
+            child: InkWell(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              onTap: onClick1,
+              child: Card(
+                color: AppColors.selectCardColor,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        choice1.pathImage,
+                        height: 45,
+                        width: 40,
+                        fit: BoxFit.fill,
+                      ),
+                      Text(
+                        choice1.title,
+                        style: const TextStyle(color: Colors.black),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -54,28 +68,31 @@ class RowTwoCardSelection extends StatelessWidget {
             height: 130,
             child: choice2 == null
                 ? const SizedBox()
-                : Card(
-                    color: AppColors.selectCardColor,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(
-                            choice2!.pathImage,
-                            height: 45,
-                            width: 40,
-                            fit: BoxFit.fill,
-                          ),
-                          Text(
-                            choice2!.title,
-                            style: const TextStyle(color: Colors.black),
-                          )
-                        ],
+                : InkWell(
+                    onTap: onClick2,
+                    child: Card(
+                      color: AppColors.selectCardColor,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              choice2!.pathImage,
+                              height: 45,
+                              width: 40,
+                              fit: BoxFit.fill,
+                            ),
+                            Text(
+                              choice2!.title,
+                              style: const TextStyle(color: Colors.black),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
