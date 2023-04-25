205ec648c67136123ec2d7ab1bea7f213a06fa9d
HienNV <hiennv@macmini.local>
2023-04-25 14:37:43 +0700

    condition

--- script-ci-to-env.sh
+++ script-ci-to-env.sh
@@ -2,7 +2,8 @@
 #!/usr/bin/env bash
 bash << EOF
 echo "$FIREBASE_APP_ID"
+echo "${FIREBASE_APP_ID}"
 echo "$CI_ENVIRONMENT_NAME"
-echo "$FIREBASE_APP_ID" >> ".env.$CI_ENVIRONMENT_NAME"
+if [ -z "${FIREBASE_APP_ID}" ]; then echo "FIREBASE_APP_ID=$FIREBASE_APP_ID" >> ".env.$CI_ENVIRONMENT_NAME"; fi
 EOF
 
